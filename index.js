4a4054fbf377a1b3e92f5384c51b1e5ff585ef9d
vladcheck <valekzhaninvladimir5@gmail.com>
2024-11-25 21:17:48 +0300

    Render data on the webpage

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 const KEY = "7WXTBFTZTF9VAREWDMKZ8VDSF";
 
+// https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+Object.defineProperty(String.prototype, "capitalize", {
+  value: function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  },
+  enumerable: false,
+});
+
+Object.defineProperty(String.prototype, "separateWords", {
+  value: function () {
+    const words = [];
+
+    for (let char of this) {
+      if (char === char.toUpperCase()) {
+        words.push(char);
+      } else {
+        words[words.length - 1] += char;
+      }
+    }
+
+    let result = "";
+    for (let word of words) {
+      result += (word === words[0] ? word : word.toLowerCase()) + " ";
+    }
+
+    return result;
+  },
+  enumerable: false,
+});
+
 async function getData(location, unitGroup = "metric") {
   try {
     const response = await fetch(
@@ -26,10 +56,20 @@ async function getData(location, unitGroup = "metric") {
   }
 }
 
+const forecastSection = document.getElementById("forecast");
 const locationInput = document.getElementById("locationInput");
 const retreiveDataBtn = document.getElementById("retreiveDataBtn");
 retreiveDataBtn.addEventListener("click", async (e) => {
   e.preventDefault();
+  forecastSection.textContent = "";
+
   const dataObj = await getData(locationInput.value);
+
+  for (let [key, value] of Object.entries(dataObj)) {
+    const p = document.createElement("p");
+    p.textContent = `${key.capitalize().separateWords()}: ${value}`;
+    forecastSection.append(p);
+  }
+
   console.log(dataObj);
 });
